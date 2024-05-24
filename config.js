be19b1ed57165116d727c6a7da9cd557eb63631a
drexmose <155102908+drexmose@users.noreply.github.com>
2024-05-24 15:33:29 +0300

    𝐉𝐄𝐒𝐓𝐄𝐑 𝐌𝐃 𝐁𝐘 𝐃𝐑𝐄𝐗

---
+++ config.js
@@ -0,0 +1,132 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const axios = require('axios');
+
+// INGAT CUY INI CUMA SC HASIL RENAME ATAU RECODE YA
+
+// Credit : @theJayy_
+// Ori : @callmevinz
+
+//=== Edit Disini 🔥
+global.namabot = "AiBotzz-MD"
+global.namaowner = "Sanjaya"
+global.packname = "Sticker by"
+global.creator = "SanjayaAds"
+global.author = "AiBotzz-MD\n\nBot WhatsApp"
+global.wm = "AiBotzz-MD"
+global.syt = "https://www.youtube.com/@SanjayaAds"
+global.sgc = "https://chat.whatsapp.com/BhLOaPSL3SKCw9QAnudLDw"
+global.idgc = "120363250058335630@g.us"
+global.email = "OwnerKuu@gmail.com"
+global.sig = "https://Instagram.com/@Jayy_yete"
+global.myweb = "https://shoplinks.to/AiBotzzSH"
+global.footer_text = "© AiBotzz-MD"
+global.owner = ['62896540577165']
+global.gifin = "https://telegra.ph/file/0cf1c5039a2be1b66ef15.mp4"
+global.thumb = "https://telegra.ph/file/6b25dc6f38b32804d7e59.jpg"
+global.thumb2 = "https://telegra.ph/file/c74dda31793574f84070e.jpg"
+global.mark = "https://telegra.ph/file/9b67cb5ed0ff661027366.jpg"
+global.themeemoji = '🌐'
+
+//=== Introgasi 🗿
+global.umurowner = "Privat" // Terserah🗿
+global.kelasowner = "Privat" // Terserah🗿
+global.statusowner = "Jomblo" // Terserah
+global.lakiapacewek = "Lanang" // Terserah
+// Kalo mau gak diisi juga gak papa
+
+//=== Payment 😋
+global.qris = "https://telegra.ph/file/6a367b58ed14c5009179d.jpg"
+global.pulsa = "https://telegra.ph/file/778689e2ede51180102ca.jpg"
+global.dana = "0838-3140-9855"
+global.gopay = "Gak ada"
+global.rek = "7605-0101-8289-536"
+
+//=== Nokos Token 😱
+global.apikey ='8c3a5b302815a138d88148fa0c5916c0595bba50' 
+
+//=== Apikey Nya 🔥
+global.lol = 'GataDios'
+global.lol = 'SGWN'
+global.rose = 'Rs-putangina'
+global.xyro = '5dRkJDWvIG'
+global.APIs = {
+xyro: "https://api.xyroinee.xyz",
+popcat : 'https://api.popcat.xyz'
+}
+// APIKeys
+global.APIKeys = {
+"https://api.xyroinee.xyz": "5dRkJDWvIG"
+}
+
+//=== Gak Tau 🐦
+global.pairingNumber = "6283831409855"
+// Nomor whatsapp bot lu
+global.prefix = ['-_-'] 
+// Jangan diubah
+global.tekspushcontact = "Izin Push" 
+// Terserah
+global.typemenu = "v6"
+// Ini type menu nya
+// v1 - v2 - v3 - v4 - v5 - v6
+global.typeallmenu = "v1"
+// Ini type allmenu nya
+// v1 - v2 - v3 - v4 - v5 - v6
+global.game = true
+// False (Nonaktifin)
+global.groupOnly = false 
+// True (Mode grup)
+global.privateOnly = false
+// True (Mode grup)
+global.antispam = true 
+// False (Nonaktifin)
+global.anticall = true
+// False (Nonaktifin)
+global.autoreadsw = true
+// False (Nonaktifin)
+global.antiBot = true
+// False (Nonaktifin)
+global.pengingat = true
+// False (Nonaktifin)
+global.autoTyping = true
+// False (Nonaktifin)
+global.autoBio = true
+// False (Nonaktifin)
+global.autoRestart = true
+// False (Nonaktifin)
+// AutoRestart Cocok Tuk Panel Butut
+global.mess = {
+ done: 'Sukses!',
+ wait: 'Sedang diproses',
+ admin: 'Fitur Khusus Admin grup!',
+ botNotAdmin: 'Saya bukan Admin grup',
+ owner: 'Fitur Khusus Owner!',
+ group: 'Fitur Khusus Group!',
+}
+
+// Setting Game
+global.gamewaktu = 60 // Game waktu
+global.bmin = 1000 // Balance minimal 
+global.bmax = 5000 // Balance maksimal
+global.limit = 30 // Limit user
+
+// Database Game
+global.suit = {};
+global.tictactoe = {};
+global.petakbom = {};
+global.kuis = {};
+global.siapakahaku = {};
+global.asahotak = {};
+global.susunkata = {};
+global.caklontong = {};
+global.family100 = {};
+global.tebaklirik = {};
+global.tebaklagu = {};
+global.tebakgambar = {};
+global.tebakkimia = {};
+global.tebakkata = {};
+global.tebakkalimat = {};
+global.tebakbendera = {};
+global.tebakanime = {};
+global.kuismath = {};
+
---
+++ readme.md
@@ -0,0 +1,44 @@
+<p align="center">
+    <img src="https://telegra.ph/file/0aa23c7f060e0919e52cf.jpg" width="100%" style="margin-left: auto;margin-right: auto;display: block;">
+    </p>
+    <h1 align="center">Vin - MD</h1>
+
+## BACA DULU SEBELUM PAKAI ##
+- Ubah Informasi di Config.js
+- Support Panel, Termux & replit
+- Pastiakan Kamu Sudah Subrek Chanel Gua
+- Harap Manuruh Credit Jika Ingin Recode Jika Tidak Di Pastikan Tidak Ada Update Lagi Ke Versi Selanjutnya.
+
+### Sedikit Tentang Bot
+- ✔️ | **Simple** 
+- ✔️ | **No Button** 
+- ✔️ | **Multi Device** 
+- ✔️ | **Work All Fitur**
+- ✔️ | **No Apikey**
+- ---------
+### Fitur Yang Dimiliki
+- ✔️ | Tools 
+- ✔️ | Remini
+- ✔️ | Quotes
+- ✔️ | Confess
+- ✔️ | Chat Gpt
+- ✔️ | Werewolf Games
+- ✔️ | And Others
+- ✔️ | Bug Warcall
+- ✔️ | Create Panel Pterodactyl
+- ✔️ | Full Fitur AI
+- ✔️ | Setmenu (v1,v2,v3,v4)
+- ✔️ | Dan Masih Banyak Lagi
+- ---------
+
+
+##
+[![Group Chat](https://img.shields.io/badge/Owner%20BOT-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://chat.whatsapp.com/BkdLVSrEZa4EviQ7COGnpT) 
+[![Bot](https://img.shields.io/badge/Bot%20Whatsapp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://wa.me/6283150418588)
+
+
+VnShoop | @callmevinz
+AiBotzzSH | @SanjayaAds
+WhatsApp Alvin : 083150418588
+WhatsApp Jay : 089654057716
+
